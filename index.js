c708762683beacf2884703502b75af5b96e61980
rg-rohit9431 <9431rohitkumar@gmail.com>
2023-12-27 09:21:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var textCreate = document.getElementById('text-box');
+    var submitButton = document.getElementById('submit');
+    var createBox = document.getElementById('create-box');
+
+    submitButton.addEventListener("click", function () {
+        if (textCreate.value.trim() === "") {
+            showNotification("Enter some tasks!","#2880e5");
+            return;
+        }
+
+        // Create box
+        var newBox = document.createElement("div");
+        newBox.className = "new-box";
+
+        // Create paragraph
+        var newPara = document.createElement("div");
+        newPara.className = "new-para";
+        newPara.innerHTML = textCreate.value;
+
+        // Create delete icon
+        var icons = document.createElement("i");
+        icons.className = "fas fa-times icon";
+
+        // Append elements
+        newBox.appendChild(newPara);
+        newBox.appendChild(icons);
+        createBox.appendChild(newBox);
+
+        // Add click event for delete
+        icons.addEventListener("click", function () {
+            showNotification("Tasks Deleted...","#fd8c73");
+            createBox.removeChild(newBox);
+        });
+
+        // Clear the input
+        textCreate.value = "";
+        showNotification("Tasks Added...","#9be9a8"); //show notification
+    });
+
+    // icons.addEventListener("click", function (event) {
+    //     if (event.target && event.target.className === "fas fa-times icon") {
+    //         var parentBox = event.target.parentNode;
+    //         createBox.removeChild(parentBox);
+    //         // showNotification("Task deleted!");
+    //     }
+    // });
+
+    function showNotification(val,coloR) {
+        var noticeBox = document.createElement("div");
+        noticeBox.className="notice";
+        noticeBox.innerHTML = val;
+
+        document.getElementById("notification").appendChild(noticeBox);
+
+        noticeBox.style.transform="translate(0,0)";
+        noticeBox.style.backgroundColor=coloR;
+        setTimeout(function () {
+            noticeBox.style.transform="translate(150%,0)";
+            document.getElementById("notification").removeChild(noticeBox);
+        }, 4000);
+        console.log(noticeBox.innerHTML);
+
+    }
+});
